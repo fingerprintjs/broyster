1527e8c742ca09f3571725b2eb6e3c868efe79c1
fpkamp <117085311+fpkamp@users.noreply.github.com>
2023-01-31 12:38:22 +0100

    add previous improvements to launcher

--- node/src/launcher.ts
+++ node/src/launcher.ts
@@ -4,8 +4,9 @@ import { BrowserStackLocalManager } from './browserstack_local_manager'
 import { BrowserStackSessionFactory } from './browserstack_session_factory'
 import { LoggerFactory } from './karma_logger'
 import { calculateHttpsPort } from './custom_servers'
-import { browserStackSessionsManager } from './browserstack_sessions_manager'
+import { BrowserStackSessionsManager } from './browserstack_sessions_manager'
 import { CustomLauncher, ConfigOptions } from 'karma'
+import { CaptureTimeout } from './capture_timeout_handler'
 
 export function BrowserStackLauncher(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -18,14 +19,13 @@ export function BrowserStackLauncher(
   retryLauncherDecorator: (arg: object) => void,
   browserStackSessionFactory: BrowserStackSessionFactory,
   browserStackLocalManager: BrowserStackLocalManager,
+  browserStackSessionsManager: BrowserStackSessionsManager,
 ) {
   baseLauncherDecorator(this)
   retryLauncherDecorator(this)
-
-  // TODO add launcher ID to help identify sessions more easily
-  // (this logger and maybe the name as the default launcher logger cannot be changed)
-  const log = logger.create('Browserstack')
+  const log = logger.create('Browserstack ' + this.id)
   const run = browserStackLocalManager.run(log)
+  const captureTimeout = new CaptureTimeout(this, config, log)
 
   this.name =
     args.browserName +
@@ -52,34 +52,22 @@ export function BrowserStackLauncher(
         clearTimeout(pendingHeartBeat)
       }
       return
-    }, 9_000)
+    }, (config.browserStack?.idleTimeout ?? 10_000) * 0.9)
   }
   this.attempt = 0
 
   this.pendingTimeoutId = null
-  const startTimeout = () => {
-    return setTimeout(() => {
-      this.pendingTimeoutId = null
-      if (this.state !== this.STATE_BEING_CAPTURED) {
-        return
-      }
-
-      log.warn(`${this.name} has not captured in ${config.captureTimeout} ms, killing.`)
-      this.error = 'timeout'
-      this.kill()
-    }, config.captureTimeout)
-  }
 
   this.on('start', async (pageUrl: string) => {
     try {
       await run
 
-      const queue = await browserStackSessionsManager.waitForQueue(config, log)
+      const queue = await browserStackSessionsManager.waitForQueue(log)
       if (!queue) {
-        throw new Error(`the BrowserStack Automate queue is at full capacity, browser ${this.id} will fail.`)
+        log.info(`the BrowserStack Automate queue is at full capacity, browser ${this.id} will fail.`)
       }
 
-      this.pendingTimeoutId = startTimeout()
+      this.pendingTimeoutId = captureTimeout.onStart()
 
       log.debug('creating browser with attributes: ' + JSON.stringify(args))
       browser = await browserStackSessionFactory.tryCreateBrowser(args, this.attempt++, log)
@@ -96,10 +84,7 @@ export function BrowserStackLauncher(
   })
 
   this.on('done', () => {
-    if (this.pendingTimeoutId) {
-      clearTimeout(this.pendingTimeoutId)
-      this.pendingTimeoutId = null
-    }
+    captureTimeout.onDone()
   })
 
   this.on('kill', async (done: () => void) => {
