1c8931ee55c075ef29dbd5d7ad74d5185f79e4ff
fpkamp <117085311+fpkamp@users.noreply.github.com>
2023-01-31 12:35:56 +0100

    encapsulate creation of BrowserStack client

--- node/src/browserstack_helpers.ts
+++ node/src/browserstack_helpers.ts
@@ -1,23 +1,36 @@
-import { createAutomateClient } from 'browserstack'
+import { AutomateClient, createAutomateClient } from 'browserstack'
 import { promisify } from 'util'
 import { Logger } from './karma_logger'
 
-export async function canNewBrowserBeQueued(log: Logger): Promise<boolean> {
-  // TODO: fix when housekeeping
-  const browserstackClient = createAutomateClient({
-    username:
-      process.env.BROWSERSTACK_USERNAME ||
-      process.env.BROWSER_STACK_USERNAME ||
-      (() => {
-        throw new Error('BrowserStack username is empty')
-      })(),
-    password:
-      process.env.BROWSERSTACK_ACCESS_KEY ||
-      process.env.BROWSER_STACK_ACCESS_KEY ||
-      (() => {
-        throw new Error('BrowserStack access key is empty')
-      })(),
+export function getBrowserStackUserName(): string {
+  const username =
+    process.env.BROWSERSTACK_USERNAME ||
+    process.env.BROWSER_STACK_USERNAME ||
+    (() => {
+      throw new Error('BrowserStack username is empty')
+    })()
+  return username
+}
+
+export function getBrowserStackAccessKey(): string {
+  const username =
+    process.env.BROWSERSTACK_ACCESS_KEY ||
+    process.env.BROWSER_STACK_ACCESS_KEY ||
+    (() => {
+      throw new Error('BrowserStack access key is empty')
+    })()
+  return username
+}
+
+export function createBrowserStackClient(): AutomateClient {
+  return createAutomateClient({
+    username: getBrowserStackUserName(),
+    password: getBrowserStackAccessKey(),
   })
+}
+
+export async function canNewBrowserBeQueued(log: Logger): Promise<boolean> {
+  const browserstackClient = createBrowserStackClient()
   log.debug('calling getPlan')
   const result = await promisify(browserstackClient.getPlan).call(browserstackClient)
   log.debug('getPlan returned:')
