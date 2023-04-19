a1ed0d7ac57fb979360abdd15b8666b427fe5acd
fpkamp <117085311+fpkamp@users.noreply.github.com>
2023-04-19 14:06:58 +0200

    use the identifier

--- node/src/browserstack_session_factory.ts
+++ node/src/browserstack_session_factory.ts
@@ -6,6 +6,7 @@ import { OptionsBuilder } from './options_builder'
 import { WebDriverFactory } from './webdriver_factory'
 import { getBrowserStackUserName, getBrowserStackAccessKey } from './browserstack_helpers'
 import { ThenableWebDriver } from 'selenium-webdriver'
+import { env } from 'process'
 
 export class BrowserStackSessionFactory {
   private _username: string
@@ -66,7 +67,7 @@ export class BrowserStackSessionFactory {
       this._idleTimeout,
       browser.osVersion,
       browser.browserVersion,
-      id,
+      env.HELLO ?? env.hello,
     )
     log.error('created capabilities: ' + JSON.stringify(caps))
     const opts = OptionsBuilder.create(browser.browserName, browser.flags)
